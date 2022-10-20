38042f247974c6ae58dd1e259f3a0d216a79acc8
Thunderbird93 <antircristian@gmail.com>
2022-10-20 14:11:06 +0200

    [add] html

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 ed emetto un messaggio in console con il numero della cella cliccata.
 */
+
+//L'utente clicca su un bottone
