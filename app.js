870378f7b3dd077a5e35e108c6249d4d038afc10
Thunderbird93 <antircristian@gmail.com>
2022-10-20 15:55:58 +0200

    [fix] css

--- app.js
+++ app.js
@@ -7,7 +7,3 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
 ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
-//L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-document.getElementById("btn").addEventListener("click", function(){
-    console.log("click");
-});
