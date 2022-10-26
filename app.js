41021f5fba43350b5d4ae4e8664c865f8056a757
Thunderbird93 <antircristian@gmail.com>
2022-10-26 21:38:27 +0200

    fin

--- app.js
+++ app.js
@@ -29,6 +29,18 @@ play.addEventListener('click', function(){
     generaCampo(celle, grid);
     function generaCampo(max, element) {
         for (let i = 1; i <= celle; i++) {
+            const cella = generaCella(i);
+
+            element.insertAdjacentElement(`beforeend`, cella);
+
+            cella.addEventListener('click', function(){
+                
+                this.classList.toggle("active");
+                console.log(this);
+
+            }
+            )
+            
         }
     }
 }
